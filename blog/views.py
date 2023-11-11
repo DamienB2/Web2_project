6a0ae1fe02d537c423b2a79896de6ddaeb16fa71
DamienB2 <damien.baise@helb-prigogine.be>
2023-11-11 21:50:30 +0100

    Part 5

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Dam',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'November 9, 2023'
-    },
-    {
-        'author': 'Bam',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 10, 2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
