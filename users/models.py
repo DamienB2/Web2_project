77ce51c712afebf3dff1f95ebda325742166ab8b
DamienB2 <damien.baise@helb-prigogine.be>
2023-11-19 17:34:08 +0100

    Part 8

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Si on ajoute quelque chose dans models, il doit y avoir un makemigrations dans la DB et ensuite un migrate
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    #Permet d'afficher un objet. Sans ça, affiche "Profile object"
+    def __str__(self):
+        return f'{self.user.username} Profile'
